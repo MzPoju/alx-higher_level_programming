12733754ea76d0da2f12645335c1c65d03492a06
Opeyemi Adepoju <adepojuoopeye@gmail.com>
2023-07-25 20:57:32 +0000

    JavaScript data format

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+# displays the status code of a request
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,6 @@
+JSON
+JavaScript Object Notation
+
+Text based format for representing data based on JavaScript object syntax.
+
+Exists as string to transmit data across a network
