1ca543d6936191b45dedea8c994d1eb4eedcac22
Opeyemi Adepoju <adepojuoopeye@gmail.com>
2023-08-07 14:30:55 +0000

    Add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-# reads and prints the content of a file
+# reads and prints the content of file
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (!err) {
+    console.log(data);
+  } else {
+    console.log(err);
+}
+}
 });
